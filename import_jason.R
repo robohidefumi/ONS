d8a8af091eb039edf49a768c0eb67934cb5b49b8
Watanabe <hidefumi.watanabe@p26077.intra.rakuten.co.jp>
2018-09-12 06:29:17 +0900

    delete network generation script

--- import_jason.R
+++ import_jason.R
@@ -35,20 +35,3 @@ write.csv(relation_df[,1:2], file = "MyData.csv",
           row.names = FALSE)
 write.csv(class_df, file="MyClass.csv",row.names = FALSE)
 
-
-net.relation <- read.csv(file="MyData.csv")
-class_map <- read.csv(file="MyClass.csv",stringsAsFactors=FALSE)
-
-net_import <- network(net.relation,
-                      directed = FALSE,
-                      matrix.type="edgelist")
-
-df_c_attribute <- data.frame(
-  src_name = net_import %v% 'vertex.names'
-)
-df_c_merged <- df_c_attribute %>% left_join(class_map,by="src_name")
-net_import <- set.vertex.attribute(net_import,"class",df_c_merged[,2])
-
-### creating adjacent matrix
-adjacent <- as.sociomatrix(net_import)
-write.csv(adjacent, file = "adjacent.csv")
