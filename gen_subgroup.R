d0074bf94ff20064d38bce79ae65e8977b76127a
Hidefumi (Robo) Watanabe <robo.hidefumi.watanabe@gmail.com>
2018-09-15 11:36:25 +0900

    subgrup with igraph again

--- gen_subgroup.R
+++ gen_subgroup.R
@@ -25,3 +25,9 @@ V(iCon)$color <- coreness + 1
 op <- par(mar = rep(0,4))
 plot(iCon,vettex.label.cex=0.8)
 par(op)
+
+colors <- rainbow(maxCoreness)
+op <- par(mar = rep(0,4))
+plot(iCon,vertex.label=coreness,
+     vertex.color=colors[coreness] )
+par(op)
