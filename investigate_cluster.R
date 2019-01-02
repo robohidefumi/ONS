9b28d65d74434117289cad0f3097fb428eeb2956
Hidefumi (Robo) Watanabe <robo.hidefumi.watanabe@gmail.com>
2019-01-02 15:21:06 +0900

    comment追加

--- investigate_cluster.R
+++ investigate_cluster.R
@@ -7,6 +7,8 @@ library(RColorBrewer)
 #### Threshold
 thr <- 25
 
+#### Import Network Data
+##### NetworkとClassデータの統合
 net.relation <- read.csv(file="MyData.csv")
 net_import <- network(net.relation,
                       directed = FALSE,
@@ -18,25 +20,29 @@ class_map <- read.csv(file="MyClass.csv",stringsAsFactors=FALSE)
 df_c_merged <- df_c_attribute %>% left_join(class_map,by="src_name")
 net_import %v% 'class' <- as.vector(df_c_merged[,2])
 
+
+#### switch from statnet to igraph
 detach(package:statnet)
 library("igraph")
 library(intergraph)
+#### creating igraph format
 iCon <- asIgraph(net_import)
-coreness <- graph.coreness(iCon)
-#table(coreness)
-maxCoreness <- max(coreness)
+#### obtaining vertex.names of iCon
 Vname <- get.vertex.attribute(iCon,name='vertex.names',
                               index=V(iCon))
+#### setting names
 V(iCon)$name <- Vname
-V(iCon)$color <- coreness + 1
+#### setting class data 
 V(iCon)$class <- as.vector(df_c_merged[,2])
 
+#### obtaining cluster data
 ceb <- cluster_edge_betweenness(iCon)
 cle <- cluster_leading_eigen(iCon)
 cl <- cluster_louvain(iCon)
 clp <- cluster_label_prop(iCon)
 ci <- cluster_infomap(iCon)
 
+#### creating data frames
 cluster_df <- data_frame(
   name = names(membership(cl)),
   class = V(iCon)$class,
@@ -49,6 +55,7 @@ cluster_df <- data_frame(
   ci = as.vector(membership(ci))
 )
 
+#### creating map master
 mst <- list(
   cluster = data.frame(
     param = c("ceb","cle","cl","clp","ci"),
@@ -56,18 +63,22 @@ mst <- list(
   )
 )
 
-## すべてのclusterの頻度を出力する
+## すべてのcluster algorithmのcluster数を出力する
 library(tidyverse)
 slide <- as.vector(mst$cluster$param) %>% map(function(x){
   
+  ### cluster algorithmごとのcluster毎のコンセプト数を取得
   cluster_count <- cluster_df %>%
     group_by_(paste(x)) %>%
     summarise(cons = n())
 
+  ### スライドタイトルを取得
+  ### 該当アルゴリズムのクラスタ名を取得
   cluster_n <- mst$cluster %>% filter(param==x) %>% select(name)
   cluster_t <- paste0("(",cluster_n$name,")")
   title_s <- paste("## Distribution of # of concepts",cluster_t,sep=" ")
-  
+
+  ### histogramを作成  
   library(ggplot2)
   g <- ggplot(cluster_count, 
               aes(cons)) + 
@@ -76,22 +87,28 @@ slide <- as.vector(mst$cluster$param) %>% map(function(x){
   list(title = title_s, plot=g)
 })
 
+#### Theresholdを超えたものだけスライドにする
 slide_thr <- as.vector(mst$cluster$param) %>% map(function(x){
-  
+
+  ### cluster algorithmごとのcluster毎のコンセプト数を取得  
   cluster_count <- cluster_df %>%
     group_by_(paste(x)) %>%
     summarise(cons = n()) %>%
     filter(cons >thr)
-  
+
+  ### スライドタイトルを取得
+  ### 該当アルゴリズムのクラスタ名を取得
   cluster_n <- mst$cluster %>% filter(param==x) %>% select(name)
   cluster_t <- paste0("(",cluster_n$name,")")
   title_s <- paste("## Distribution of # of concepts",cluster_t,sep=" ")
-  
+
+  ### histogramを作成    
   library(ggplot2)
   g <- ggplot(cluster_count, 
               aes(cons)) + 
     geom_histogram()
-  
+
+  ### histogramを作成  
   if(length(cluster_count$cons) > 0){
     list(title = title_s, plot=g)
   }
@@ -105,21 +122,28 @@ cl_list <- list(cluster_edge_betweenness,
                 cluster_infomap) %>%
             map(~ .(iCon))
 
+#### argorithm毎にconcept毎のcluster番号
 list_cluster <- seq_along(cl_list) %>% map(function(x){
   data.frame(
+    # concept名
     name = names(membership(cl_list[[x]])),
+    # cluster番号
     cl_no = as.vector(membership(cl_list[[x]])),
+    # cluster名を出力
     cluster = rep(as.vector(mst$cluster$name[x]),length(membership(cl_list[[x]])))
   )
 })
+### 一つのデータフレムに結合
 df_cluster <- list.rbind(list_cluster)
 
+### 閾値以上のコンセプト数が存在するクラスター番号のみのコンセプト数を出力
 df_count_thr <- df_cluster %>%
 #  filter(cluster == "Edge Betweenness") %>%
   group_by(cluster,cl_no) %>%
   summarise(cons = n()) %>%
   filter(cons > thr)
 
+### 閾値以上のコンセプト数が存在するクラスター番号のみのデータフレームを作成する
 x <- as.vector(df_count_thr$cluster)
 y <- as.vector(df_count_thr$cl_no)
 list_con_thr <- map2(x,y, function(x,y){
@@ -128,8 +152,10 @@ list_con_thr <- map2(x,y, function(x,y){
     select(cluster, cl_no, name)
 })
 df_con_thr <- list.rbind(list_con_thr)
+### class中心性を追加
 df_con_thr <- df_con_thr %>% left_join(cluster_df[,c("name","class","bet","deg")],by="name")
 library(knitr)
+### 表を作成
 k_list <- map2(x,y, function(x,y){
   table <- df_con_thr %>%
     filter(cluster==x & cl_no == y) %>%
